97c62b2795dc58b347b585946c176d4b3803a23d
Laven Sukumaran <laven@tenkeylabs.com>
2023-11-21 13:52:14 -0500

    Fix capitalization definition rules

--- types/conversionmechanisms/ConversionMechanism.go
+++ types/conversionmechanisms/ConversionMechanism.go
@@ -1,6 +1,9 @@
 package conversionmechanisms
 
-import "github.com/tenkeylabs/go-ocf/enums"
+import (
+	"github.com/tenkeylabs/go-ocf/enums"
+	"github.com/tenkeylabs/go-ocf/types"
+)
 
 type ConversionMechanism struct {
 	*CustomConversionMechanism
@@ -15,4 +18,7 @@ type ConversionMechanism struct {
 	Type enums.ConversionMechanismType `json:"type"` //TODO: Remove type from each conversion mechanism
 	// Is this an MFN (Most Favored Nations) flavored Convertible Note?
 	ConversionMfn *bool `json:"conversion_mfn,omitempty"`
+	// The rules for which types of securities would be included in the capitalization
+	// definition.
+	CapitalizationDefinitionRules *types.CapitalizationDefinitionRules `json:"capitalization_definition_rules,omitempty"`
 }
